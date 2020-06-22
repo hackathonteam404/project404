a0468fd609b11509fa01f3204722b42c820422ab
Vietfu Tang <29468130+vietfu@users.noreply.github.com>
2020-06-22 19:10:31 -0400

    Initial commit

---
+++ src/main/java/com/hackathonteam404/project404/Project404Application.java
@@ -0,0 +1,13 @@
+package com.hackathonteam404.project404;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project404Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Project404Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/hackathonteam404/project404/Project404ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackathonteam404.project404;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project404ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
