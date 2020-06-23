ee9f7d19adc9677c0c26899d0341f5e7b914f193
Vietfu Tang <29468130+vietfu@users.noreply.github.com>
2020-06-22 20:22:09 -0400

    Added example model and controller, and implemented API calling from client

--- client/src/App.js
+++ client/src/App.js
@@ -2,25 +2,36 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  getGreeting = async () => {
+    const response = await fetch('http://localhost:8080/greeting');
+    const body = await response.json();
+    this.setState({
+      id: body.id,
+      content: body.content,
+    });
+  }
+
+  componentDidMount() {
+    this.getGreeting();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>Id: {this.state.id}</p>
+          <p>Content: {this.state.content}</p>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/main/java/com/hackathonteam404/project404/Project404Application.java
+++ src/main/java/com/hackathonteam404/project404/Project404Application.java
@@ -2,12 +2,25 @@ package com.hackathonteam404.project404;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class Project404Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Project404Application.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/greeting").allowedOrigins("http://localhost:3000");
+            }
+        };
     }
 
 }
