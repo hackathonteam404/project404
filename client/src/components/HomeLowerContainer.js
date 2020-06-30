8c44b97dcf424ff67f956d845c6907f91bdd021f
Vietfu Tang <29468130+vietfu@users.noreply.github.com>
2020-06-30 02:44:49 -0400

    Added basic dropdown for future filtering

--- client/src/components/HomeLowerContainer.js
+++ client/src/components/HomeLowerContainer.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import Map from './Map';
 import StoreInfoCard from './StoreInfoCard';
-import { Container, Row, Col, ListGroup } from 'react-bootstrap';
+import { Container, Row, Col, ListGroup, Dropdown, DropdownButton } from 'react-bootstrap';
 import '../index.scss';
 
 export class HomeLowerContainer extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: "Restaurants"
+        }
+    }
     render() {
         const stores = []
         for (let i = 0; i < 8; i++) {
@@ -25,7 +31,12 @@ export class HomeLowerContainer extends React.Component {
                         <Map center={this.props.center}/>
                     </Col>
                     <Col>
-                    <ListGroup style={{overflow: 'scroll', overflowX: 'hidden', maxHeight: '100vh'}}>
+                    <DropdownButton id="dropdown-basic-button" title={this.state.title}>
+                        <Dropdown.Item onClick={() => this.setState({title: 'Restaurants'})}>Restaurants</Dropdown.Item>
+                        <Dropdown.Item onClick={() => this.setState({title: 'Grocery Stores'})}>Grocery Stores</Dropdown.Item>
+                        <Dropdown.Item onClick={() => this.setState({title: 'Pharmacies'})}>Pharmacies</Dropdown.Item>
+                    </DropdownButton>
+                    <ListGroup style={{overflow: 'scroll', overflowX: 'hidden', maxHeight: '95vh'}}>
                         {stores}
                     </ListGroup>
                     </Col>
