648b7894f89fb98aa562b459bad127df036571c1
Alice Han <55503673+amh2sd@users.noreply.github.com>
2020-07-01 15:31:28 -0400

    names

--- client/src/components/NavigationBar.js
+++ client/src/components/NavigationBar.js
@@ -6,12 +6,11 @@ export class NavigationBar extends React.Component {
     render() {
         return (
             <Navbar bg="light" expand="lg" className="shadow-sm sticky-top">
-                <Navbar.Brand href="#home">Project 404</Navbar.Brand>
+                <Navbar.Brand href="#home">Merchant Queue Times</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="mr-auto">
-                        <Nav.Link href="#p1">Store Search</Nav.Link>
-                        <Nav.Link href="#p2">About</Nav.Link>
+                        <Nav.Link href="#p1">About</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
